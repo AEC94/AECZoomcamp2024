eb6775de00d71667fbee4dfabec463312380f96f
Agustín Ezequiel Cipriano <143001965+AEC94@users.noreply.github.com>
2024-02-04 15:32:54 +0000

    Adding week2

--- sql_scripts/ex_4.sql
+++
@@ -1,7 +0,0 @@
-SELECT tz."Borough",SUM(gt.total_amount) AS total_amount
-FROM green_taxi_trips gt
-LEFT JOIN taxi_zones tz ON gt."PULocationID"  = tz."LocationID"
-WHERE DATE(gt.lpep_pickup_datetime) = '2019-09-18' AND tz."Borough" <> 'Unknown'
-GROUP BY tz."Borough"
-HAVING total_amount > 50000
-ORDER BY total_amount DESC
