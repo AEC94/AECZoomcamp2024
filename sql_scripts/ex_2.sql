28170593b17b531d9243fa82cdacc2a4ef98e194
Agustín Ezequiel Cipriano <143001965+AEC94@users.noreply.github.com>
2024-02-08 00:47:11 +0000

    Adding queries

--- sql_scripts/ex_2.sql
+++
@@ -1,3 +0,0 @@
-SELECT COUNT(*)
-FROM green_taxi_trips
-WHERE DATE(lpep_pickup_datetime) = '2019-09-18' AND DATE(lpep_dropoff_datetime) = '2019-09-18'
--- sql_scripts/ex_3.sql
+++
@@ -1,5 +0,0 @@
-SELECT DATE(lpep_pickup_datetime) AS pickup_date, MAX(trip_distance) AS max_trip_distance
-FROM green_taxi_trips
-GROUP BY pickup_date
-ORDER BY max_trip_distance DESC
-LIMIT 1
